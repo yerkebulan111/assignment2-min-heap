c5b7bb7cf79c09a17b593b218e1a2c5f4d842b59
yerkebulan <abdisattarerkebulan@gmail.com>
2025-10-05 20:29:32 +0500

    feat(algorithm): baseline MinHeapSort implementation

---
+++ src/main/java/com/company/algorithms/MinHeapSort.java
@@ -0,0 +1,48 @@
+package com.company.algorithms;
+
+public class MinHeapSort {
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int smallest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        // If left child is smaller than root
+        if (left < n && arr[left] < arr[smallest]) {
+            smallest = left;
+        }
+
+        // If right child is smaller than smallest so far
+        if (right < n && arr[right] < arr[smallest]) {
+            smallest = right;
+        }
+
+        // If smallest is not root
+        if (smallest != i) {
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+
+
+            heapify(arr, n, smallest);
+        }
+    }
+
+}
